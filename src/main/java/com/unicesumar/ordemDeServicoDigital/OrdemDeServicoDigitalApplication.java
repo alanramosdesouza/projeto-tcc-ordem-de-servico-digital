6e7d6918c56a3c3c6ce0c2f4fcccf9fff539e63b
Alan Ramos <alanramosdesouza@gmail.com>
2023-04-21 16:04:32 -0300

    Project created

---
+++ src/main/java/com/unicesumar/ordemDeServicoDigital/OrdemDeServicoDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.unicesumar.ordemDeServicoDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemDeServicoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemDeServicoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unicesumar/ordemDeServicoDigital/OrdemDeServicoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unicesumar.ordemDeServicoDigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemDeServicoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
